60750a024cec2f9d91d0b6373a57f8764703d34d
fulopd <fulopdaniel@gmail.com>
2020-04-18 10:16:54 +0200

    Add project files.

---
+++ Nyelvvizsga/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyelvvizsga
+{
+    class Program
+    {
+        static private List<Vizsga> nyelvvizygaLista = new List<Vizsga>();
+        static void Main(string[] args)
+        {
+
+            #region 1. feladat
+            void BeolvasSikeres()
+            {
+
+                using (StreamReader sr = new StreamReader("Forras/sikeres.csv"))
+                {
+                    string[] fejlec = sr.ReadLine().Split(';');
+                    while (!sr.EndOfStream)
+                    {
+                        string[] sorTomb = sr.ReadLine().Split(';');
+                        Vizsga nyelvvizsga = new Vizsga(sorTomb[0]);
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[1]), Convert.ToInt32(sorTomb[1]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[2]), Convert.ToInt32(sorTomb[2]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[3]), Convert.ToInt32(sorTomb[3]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[4]), Convert.ToInt32(sorTomb[4]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[5]), Convert.ToInt32(sorTomb[5]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[6]), Convert.ToInt32(sorTomb[6]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[7]), Convert.ToInt32(sorTomb[7]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[8]), Convert.ToInt32(sorTomb[8]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[9]), Convert.ToInt32(sorTomb[9]));
+                        nyelvvizsga.sikeresVizsgak.Add(short.Parse(fejlec[10]), Convert.ToInt32(sorTomb[10]));
+
+
+                        nyelvvizygaLista.Add(nyelvvizsga);
+                    }
+                }
+
+            }
+
+            void BeolvasasSikertelen()
+            {
+                using (StreamReader sr = new StreamReader("Forras/sikertelen.csv"))
+                {
+                    string[] fejlec = sr.ReadLine().Split(';');
+                    while (!sr.EndOfStream)
+                    {
+                        var sorTomb = sr.ReadLine().Split(';');
+                        var nyelvvizsga = nyelvvizygaLista.SingleOrDefault(x => x.Nyelv == sorTomb[0]);
+
+                        short evszam = 2009;
+                        for (int i = 1; i < sorTomb.Length; i++)
+                        {
+                            var dbszam = Convert.ToInt32(sorTomb[i]);
+                            //sikertelen listahoz hozzáadás
+                            nyelvvizsga.sikerestelenVizsgak.Add(evszam, dbszam);
+                            evszam++;
+                        }
+                    }
+                }
+            }
+
+            BeolvasSikeres();
+            BeolvasasSikertelen();
+
+            #endregion
+
+            #region 2. feladat
+            Console.WriteLine("2. feladat: A legnépszerűbb nyelvek:");
+
+            var nepszeruLista = nyelvvizygaLista.OrderByDescending(x => x.OsszesVizsgazo).ToList();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("\t" + nepszeruLista[i].Nyelv);
+            }
+
+            #endregion
+
+            #region 3. feladat
+            Console.WriteLine("3. feladat:");
+            short bekertEvszam = 0;
+            do
+            {
+                Console.Write("\tVizsgálandó év: ");
+                short.TryParse(Console.ReadLine(), out bekertEvszam);
+
+            } while (bekertEvszam > 2018 || bekertEvszam < 2009);
+            #endregion
+
+            #region 4. feladat
+            Console.WriteLine("4. feladat:");
+            var sikertelenebb = nyelvvizygaLista.OrderByDescending(x => x.SikertelenAtlagEvszerint(bekertEvszam)).First();
+            Console.WriteLine($"\t{bekertEvszam}. évben " +
+                $"{sikertelenebb.Nyelv} nyelvből a sikertelen vizsgák aránya: " +
+                $"{sikertelenebb.SikertelenAtlagEvszerint(bekertEvszam)}%");
+            #endregion
+
+            #region 5. feladat
+            Console.WriteLine("5. feladat:");
+            var vizsgazoSzam = nyelvvizygaLista
+                .Where(x => x.VizsgazokEvszerint(bekertEvszam).Equals(0))
+                //foreach LINQ -hoz kell
+                .ToList();
+
+            if (vizsgazoSzam.Any())
+            {
+                vizsgazoSzam.ForEach(x => Console.WriteLine("\t" + x.Nyelv));
+            }
+            else
+            {
+                Console.WriteLine("\tMinden nyelvből volt vizsgázó!");
+            }
+
+            #endregion
+
+            #region 6. feladat
+            using (StreamWriter sw = new StreamWriter("kimenet.csv", false, Encoding.UTF8))
+            {
+                foreach (var item in nyelvvizygaLista)
+                {
+                    var atlag = item.sikeresAtlag().ToString().Replace(',', '.');
+                    sw.WriteLine($"{item.Nyelv};{item.OsszesVizsgazo};{atlag}%");
+                }
+
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nyelvvizsga/Vizsga.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyelvvizsga
+{
+    class Vizsga
+    {
+     
+
+        //nyelv;2009;2010;2011;2012;2013;2014;2015;2016;2017;2018
+        //angol;336;231;258;350;93;194;128;308;198;174
+
+        public string Nyelv { get; set; }
+
+        public Dictionary<short,int> sikeresVizsgak { get; set; }
+        public Dictionary<short, int> sikerestelenVizsgak { get; set; }
+
+        public Vizsga(string nyelv)
+        {
+            Nyelv = nyelv;
+            this.sikeresVizsgak = new Dictionary<short, int>();
+            this.sikerestelenVizsgak = new Dictionary<short, int>();
+
+        }
+
+
+        public int OsszesVizsgazo 
+        {
+            get 
+            {
+                int db = 0;
+                foreach (int item in sikerestelenVizsgak.Values)
+                {
+                    db += item;
+                }
+                foreach (int item in sikeresVizsgak.Values)
+                {
+                    db += item;
+                }
+                return db;
+            }
+        }
+
+        public double SikertelenAtlagEvszerint(short evszam)
+        {
+            double siker = sikeresVizsgak[evszam];
+            double sikertelen = sikerestelenVizsgak[evszam];
+            if (siker + sikertelen == 0)
+            {
+                return 0;
+            }
+            var eredmeny = sikertelen / (sikertelen + siker) * 100;
+            return Math.Round(eredmeny,2);
+        }
+
+
+        public int VizsgazokEvszerint(short evszam)
+        {
+            return sikeresVizsgak[evszam] + sikerestelenVizsgak[evszam];
+        }
+
+        public double sikeresAtlag()
+        {
+            double siker = sikeresVizsgak.Sum(x => x.Value);
+            double sikertelen = sikerestelenVizsgak.Sum(x => x.Value);
+            if (siker + sikertelen == 0)
+            {
+                return 0;
+            }
+            var eredmeny = siker / (sikertelen + siker) * 100;
+            return Math.Round(eredmeny, 2);
+
+        }
+
+    }
+}
